13533312183a32854579f3abdd7045fba0cc9b95
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-12-27 11:51:31 -0300

    Preenchendo Dados

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -10,11 +10,27 @@ describe('Cadastro', ()=>{
             nome: "Yuri Anderson",
             cpf: "111.222.333.65",
             email:'yuri@teste.com',
-            whatsapp:' 1199999999'
+            whatsapp:' 1199999999',
+            endereco:{
+                cep:'04534011',
+                rua: 'Rua Joaquim Floreano',
+                numero:'1000',
+                complemento:'Ap 201',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP'
+            }
+
         }
         cy.get('input[name="name"]').type(entregador.nome)
         cy.get('input[name="cpf"]').type(entregador.cpf)
         cy.get('input[name="email"]').type(entregador.email)
         cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+        
     })
 })
