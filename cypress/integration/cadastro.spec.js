c664101cb8b46b581d8f5b0b403a0c46fca006f7
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-12-28 23:46:16 -0300

    Combinando Css e seletor de texto

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -18,7 +18,8 @@ describe('Cadastro', ()=>{
                 complemento:'Ap 201',
                 bairro: 'Itaim Bibi',
                 cidade_uf: 'São Paulo/SP'
-            }
+            },
+            metodo_entrega: 'Moto'
 
         }
         cy.get('input[name="name"]').type(entregador.nome)
@@ -37,6 +38,6 @@ describe('Cadastro', ()=>{
         cy.get('input[name="district"]').should('have.value',entregador.endereco.bairro)
         cy.get('input[name="city-uf"]').should('have.value',entregador.endereco.cidade_uf)
         
-        
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
     })
 })
