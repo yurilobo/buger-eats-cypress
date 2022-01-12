1a7595c75a880268d1e74931a83d82ab02c0b2c7
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-01-11 23:38:03 -0300

    Viewport e Base url

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe('home page', ()=>{
     it('app deve esta online',()=>{
-        cy.viewport(1440,900)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('h1').should('have.text','Seja um parceiro entregador pela Buger Eats')
     })
 })
