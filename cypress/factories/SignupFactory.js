8964bfb58b5e85cebbe0ff0570e63afcd21d7626
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-01-20 00:00:45 -0300

    Faker

--- cypress/factories/SignupFactory.js
+++ cypress/factories/SignupFactory.js
@@ -1,9 +1,15 @@
+var faker = require('faker')
+var cpf = require('gerador-validador-cpf')
+
 export default {
     deliver: function () {
+        var firstName = faker.name.firstName()
+        var lastName = faker.name.lastName()
+
         var data = {
-            name: 'Yuri Anderson',
-            cpf: '08709260048',
-            email: 'yuri@teste.com',
+            name: `${firstName} ${lastName}`,
+            cpf: cpf.generate(),
+            email: faker.internet.email(firstName),
             whatsapp: ' 1199999999',
             address: {
                 postalcode: '04534011',
